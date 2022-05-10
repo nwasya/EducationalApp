44edf5c56570f194509dd108f6d97a8cfd0b8eff
aysan <eshraghi@gmail.com>
2022-05-10 23:57:36 +0430

    enter and edit mark functions has changed

--- Edu_Mark/urls.py
+++ Edu_Mark/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from Edu_Mark.views import enter_mark_page, final_report, view_student_report, edit_mark_page
+from Edu_Mark.views import enter_mark_page, final_report, view_student_report, edit_mark_page, \
+    enter_mark_choose_course_page, enter_mark_choose_student_page, edit_mark_choose_course_page, \
+    edit_mark_choose_student_page
 
 urlpatterns = [
-    path('add-mark', enter_mark_page, name='enter-mark'),
+    path('add-mark-cc', enter_mark_choose_course_page, name='enter-mark-choose-course'),
+    path('add-mark-cs/<course_id>', enter_mark_choose_student_page, name='enter-mark-choose-student'),
+    path('add-mark/<course_id>/<student_id>', enter_mark_page, name='enter-mark'),
     path('final-report/<course_id>', final_report, name='final-report'),
     path('view-student-report', view_student_report, name='view_student_report'),
-    path('edit-mark', edit_mark_page, name='edit_mark'),
+    path('edit-mark-cc', edit_mark_choose_course_page, name='edit-mark-choose-course'),
+    path('edit-mark-cs/<course_id>', edit_mark_choose_student_page, name='edit-mark-choose-student'),
+    path('edit-mark/<course_id>/<student_id>', edit_mark_page, name='edit-mark'),
 
 ]
--- Edu_Mark/views.py
+++ Edu_Mark/views.py
@@ -10,102 +10,11 @@ from Edu_course.models import Course
 from Edu_teacher.models import TeacherClass
 from Edu_user.models import UserProfile, Student
 
-
-# test git
-
 @login_required(login_url='/login')
-def enter_mark_page(request):
-    global course_name, student_name
+def enter_mark_choose_course_page(request):
     if UserProfile.objects.get(user__username=request.user.username).role != 'Teacher':
         return redirect('/')
-    context = {
-        'course_flag': True,
-        'student_flag': False,
-        'mark_flag': False,
-
-    }
-    mark_form = CreateMarkForm()
-    if request.method == 'POST':
-
-        print(request.POST)
-
-        if 'course' in request.POST:
-            print("entexab")
-            course_id = request.POST['course']
-            course_name = Course.objects.get(id=course_id)
-            student = Student.objects.filter(course__id=course_id)
-
-            context['student'] = student
-            context['student_flag'] = True
-            context['course_flag'] = False
-
-        if 'student' in request.POST:
-            print("vared")
-            context['student_flag'] = False
-            context['course_flag'] = False
-            context['mark_flag'] = True
-            id_num = request.POST['student']
-
-            mark_form = CreateMarkForm()
-            student_name = Student.objects.get(id_num=id_num)
-
-            context['full_name'] = f"{student_name.first_name} {student_name.last_name}"
-            context['class_name'] = course_name.title
-            x = Mark.objects.filter(student_name=student_name, course_name=course_name).exists()
-            if x:
-                student = Student.objects.filter(course=course_name)
-                context['student'] = student
-                messages.info(request,
-                              "برای این دانش آموز قبلا نمره ای ثبت شده است. می توانید از پنل ویرایش نمره آن را ویرایش کنید")
-                context['student_flag'] = True
-                context['course_flag'] = False
-                context['mark_flag'] = False
-                user_id = request.user.username
-
-                teacher = TeacherClass.objects.filter(id_num=user_id).first()
-
-                course = Course.objects.filter(teacher=teacher, active=True)
-                context['course'] = course
-
-                context['form'] = mark_form
-                return render(request, 'enter_mark.html', context)
-
-        if 'send' in request.POST:
-            mark_form = CreateMarkForm(request.POST)
-
-            if mark_form.is_valid():
-
-                class_activity = mark_form.cleaned_data['class_activity']
-                midterm = mark_form.cleaned_data['midterm']
-                final = mark_form.cleaned_data['final']
-                quizzes = mark_form.cleaned_data['quizzes']
-                extra_mark = mark_form.cleaned_data['extra_mark']
-                total_mark = mark_form.cleaned_data['total_mark']
-                total_work = request.POST['total_work']
-                description = mark_form.cleaned_data['description']
-                writing = request.POST['writing']
-                homework = request.POST['homework']
-                listening = request.POST['listening']
-                speaking = request.POST['speaking']
-                activity = request.POST['activity']
-
-                x = Mark.objects.create(student_name=student_name, course_name=course_name,
-                                        class_activity=class_activity,
-                                        quizzes=quizzes, midterm=midterm, final=final,
-                                        extra_mark=extra_mark, total_mark=total_mark,
-                                        total_work=total_work,
-                                        description=description, activity=activity,
-                                        speaking=speaking, listening=listening,
-                                        writing=writing, homework=homework,
-                                        )
-                x.save()
-                student_name.is_registered = False
-                student_name.save()
-
-                messages.info(request, "نمره با موفقیت وارد شد")
-                return HttpResponseRedirect('/add-mark')
-            else:
-                messages.error(request, 'خطا در ثبت نمره')
+    context = {}
 
     user_id = request.user.username
 
@@ -113,9 +22,216 @@ def enter_mark_page(request):
 
     course = Course.objects.filter(teacher=teacher, active=True)
     context['course'] = course
+    if request.method == 'POST':
+        course_id = request.POST['course']
+
+        return redirect(f"/add-mark-cs/{course_id}")
+
+
+
+    return render(request, 'enter_mark_choose_course.html', context)
+
+@login_required(login_url='/login')
+def enter_mark_choose_student_page(request, *args, **kwargs):
+    course_id = kwargs["course_id"]
+    context = {}
+    course_name = Course.objects.get(id=course_id)
+
+    student = Student.objects.filter(course__id=course_id)
+    context['student'] = student
+
+    context['student'] = student
+    if request.method == "POST":
+        student_id = request.POST['student']
+        student_name = Student.objects.get(id=student_id)
+        x = Mark.objects.filter(student_name=student_name, course_name=course_name).exists()
+        if x:
+            messages.info(request,
+                          "برای این دانش آموز قبلا نمره ای ثبت شده است. می توانید از پنل ویرایش نمره آن را ویرایش کنید")
+            return render(request, 'enter_mark_choose_student.html', context)
+
+        else:
+            return redirect(f"/add-mark/{course_id}/{student_id}")
+    return render(request, 'enter_mark_choose_student.html', context)
+
+
+@login_required(login_url='/login')
+def enter_mark_page(request, *args, **kwargs):
+    context = {}
+    course_id = kwargs["course_id"]
+    student_id = kwargs["student_id"]
+    course_name = Course.objects.get(id=course_id)
+    student_name = Student.objects.get(id=int(student_id))
+    mark_form = CreateMarkForm()
+
+    context['full_name'] = f"{student_name.first_name} {student_name.last_name}"
+    context['class_name'] = course_name.title
 
     context['form'] = mark_form
+
+    if request.method == "POST":
+        mark_form = CreateMarkForm(request.POST)
+
+        if mark_form.is_valid():
+
+            class_activity = mark_form.cleaned_data['class_activity']
+            midterm = mark_form.cleaned_data['midterm']
+            final = mark_form.cleaned_data['final']
+            quizzes = mark_form.cleaned_data['quizzes']
+            extra_mark = mark_form.cleaned_data['extra_mark']
+            total_mark = mark_form.cleaned_data['total_mark']
+            total_work = request.POST['total_work']
+            description = mark_form.cleaned_data['description']
+            writing = request.POST['writing']
+            homework = request.POST['homework']
+            listening = request.POST['listening']
+            speaking = request.POST['speaking']
+            activity = request.POST['activity']
+            user_id = request.user.username
+            teacher = TeacherClass.objects.get(id_num=user_id).first()
+            x = Mark.objects.create(student_name=student_name, course_name=course_name,
+                                    class_activity=class_activity,
+                                    quizzes=quizzes, midterm=midterm, final=final,
+                                    extra_mark=extra_mark, total_mark=total_mark,
+                                    total_work=total_work,
+                                    description=description, activity=activity,
+                                    speaking=speaking, listening=listening,
+                                    writing=writing, homework=homework,teacher=teacher
+                                    )
+            x.save()
+            student_name.is_registered = False
+            student_name.save()
+
+            messages.info(request, "نمره با موفقیت وارد شد")
+            return HttpResponseRedirect('/add-mark')
+        else:
+            messages.error(request, 'خطا در ثبت نمره')
+
     return render(request, 'enter_mark.html', context)
+
+
+
+
+
+
+
+
+@login_required(login_url='/login')
+def edit_mark_choose_course_page(request):
+    if UserProfile.objects.get(user__username=request.user.username).role != 'Teacher':
+        return redirect('/')
+    context = {}
+
+    user_id = request.user.username
+
+    teacher = TeacherClass.objects.filter(id_num=user_id).first()
+
+    course = Course.objects.filter(teacher=teacher, active=True)
+    context['course'] = course
+    if request.method == 'POST':
+        course_id = request.POST['course']
+
+        return redirect(f"/edit-mark-cs/{course_id}")
+
+
+
+    return render(request, 'edit_mark_choose_course.html', context)
+
+
+@login_required(login_url='/login')
+def edit_mark_choose_student_page(request, *args, **kwargs):
+    course_id = kwargs["course_id"]
+
+    context = {}
+    course_name = Course.objects.get(id=course_id)
+
+    student = Student.objects.filter(course__id=course_id)
+    context['student'] = student
+
+    if request.method == "POST":
+        student_id = request.POST['student']
+        marks = Mark.objects.filter(student_name__id=int(student_id), course_name=course_name).first()
+
+        if marks is None:
+            messages.error(request, "نمره برای این دانش آموز ثبت نگردیده است")
+        else:
+
+            return redirect(f"/edit-mark/{course_id}/{student_id}")
+
+    return render(request, 'edit_mark_choose_student.html', context)
+
+
+
+@login_required(login_url='/login')
+def edit_mark_page(request, *args, **kwargs):
+    context = {}
+    course_id = kwargs["course_id"]
+    student_id = kwargs["student_id"]
+    course_name = Course.objects.get(id=course_id)
+    student_name = Student.objects.get(id=int(student_id))
+    context['full_name'] = f"{student_name.first_name} {student_name.last_name}"
+    context['class_name'] = course_name.title
+    marks = Mark.objects.filter(student_name__id=student_id, course_name=course_name).first()
+    if marks is not None:
+
+        edit_mark_form = CreateMarkForm(
+            initial={'class_activity': marks.class_activity,
+                     'quizzes': marks.quizzes,
+                     'midterm': marks.midterm,
+                     'final': marks.final,
+                     'extra_mark': marks.extra_mark,
+                     'total_mark': marks.total_mark,
+                     'description': marks.description,
+                     }
+        )
+        context['form'] = edit_mark_form
+
+
+    if request.method == "POST":
+        edit_mark_form = CreateMarkForm(request.POST)
+
+        if edit_mark_form.is_valid():
+            class_activity = edit_mark_form.cleaned_data['class_activity']
+            midterm = edit_mark_form.cleaned_data['midterm']
+            final = edit_mark_form.cleaned_data['final']
+            quizzes = edit_mark_form.cleaned_data['quizzes']
+            extra_mark = edit_mark_form.cleaned_data['extra_mark']
+            total_mark = edit_mark_form.cleaned_data['total_mark']
+            total_work = request.POST['total_work']
+            description = edit_mark_form.cleaned_data['description']
+            writing = request.POST['writing']
+            homework = request.POST['homework']
+            listening = request.POST['listening']
+            speaking = request.POST['speaking']
+            activity = request.POST['activity']
+            student_name = Student.objects.get(id=student_id)
+            user_id = request.user.username
+
+            teacher = TeacherClass.objects.filter(id_num=user_id).first()
+            y = Mark.objects.filter(student_name=student_name, course_name=course_name).first()
+            y.delete()
+            x = Mark.objects.create(student_name=student_name, course_name=course_name,
+                                    class_activity=class_activity,
+                                    quizzes=quizzes, midterm=midterm, final=final,
+                                    extra_mark=extra_mark, total_mark=total_mark,
+                                    total_work=total_work,
+                                    description=description, activity=activity,
+                                    speaking=speaking, listening=listening,
+                                    writing=writing, homework=homework,teacher=teacher
+                                    )
+
+            x.save()
+            messages.success(request, "نمرات با موفیقت ویرایش شد")
+            return redirect(f"/edit-mark-cs/{course_id}")
+
+    return render(request, 'edit_mark_page.html', context)
+
+
+
+
+
+
+
 
 
 @login_required(login_url='/login')
@@ -182,106 +298,6 @@ def view_student_report(request):
 
     context['form'] = mark_form
     return render(request, 'view_student_report.html', context)
-
-
-@login_required(login_url='/login')
-def edit_mark_page(request):
-    global course_name, id_num
-    if UserProfile.objects.get(user__username=request.user.username).role != 'Teacher':
-        return redirect('/')
-    context = {}
-    context['course_flag'] = True
-
-    if request.method == 'POST':
-
-        if 'course' in request.POST:
-            print("entexab")
-            course_id = request.POST['course']
-
-            course_name = Course.objects.get(id=course_id)
-            student = Student.objects.filter(course__id=course_id)
-
-            context['student'] = student
-            context['student_flag'] = True
-            context['course_flag'] = False
-
-        if 'student' in request.POST:
-            id_num = request.POST['student']
-
-            marks = Mark.objects.filter(student_name__id_num=id_num, course_name=course_name).first()
-
-            if marks is not None:
-                student_name = Student.objects.get(id_num=id_num)
-                edit_mark_form = CreateMarkForm(
-                    initial={'class_activity': marks.class_activity,
-                             'quizzes': marks.quizzes,
-                             'midterm': marks.midterm,
-                             'final': marks.final,
-                             'extra_mark': marks.extra_mark,
-                             'total_mark': marks.total_mark,
-                             'description': marks.description,
-                             }
-                )
-                full_Name = f'{student_name.first_name} {student_name.last_name}'
-                context['full_name'] = full_Name
-                context["class_name"] = course_name.title
-
-                context['form'] = edit_mark_form
-                context['student_flag'] = False
-                context['course_flag'] = False
-                context['mark_flag'] = True
-            else:
-                context['mark_flag'] = False
-                context['student_flag'] = False
-                context['course_flag'] = True
-
-                messages.error(request, "نمره برای این دانش آموز ثبت نگردیده است")
-
-        if 'ارسال' in request.POST:
-
-            edit_mark_form = CreateMarkForm(request.POST)
-
-            if edit_mark_form.is_valid():
-                class_activity = edit_mark_form.cleaned_data['class_activity']
-                midterm = edit_mark_form.cleaned_data['midterm']
-                final = edit_mark_form.cleaned_data['final']
-                quizzes = edit_mark_form.cleaned_data['quizzes']
-                extra_mark = edit_mark_form.cleaned_data['extra_mark']
-                total_mark = edit_mark_form.cleaned_data['total_mark']
-                total_work = request.POST['total_work']
-                description = edit_mark_form.cleaned_data['description']
-                writing = request.POST['writing']
-                homework = request.POST['homework']
-                listening = request.POST['listening']
-                speaking = request.POST['speaking']
-                activity = request.POST['activity']
-                student_name = Student.objects.get(id_num=id_num)
-
-                y = Mark.objects.filter(student_name=student_name, course_name=course_name).first()
-                y.delete()
-                x = Mark.objects.create(student_name=student_name, course_name=course_name,
-                                        class_activity=class_activity,
-                                        quizzes=quizzes, midterm=midterm, final=final,
-                                        extra_mark=extra_mark, total_mark=total_mark,
-                                        total_work=total_work,
-                                        description=description, activity=activity,
-                                        speaking=speaking, listening=listening,
-                                        writing=writing, homework=homework,
-                                        )
-
-                x.save()
-                messages.success(request, "نمرات با موفیقت ویرایش شد")
-                return redirect('/edit-mark')
-
-    user_id = request.user.username
-
-    teacher = TeacherClass.objects.filter(id_num=user_id).first()
-
-    course = Course.objects.filter(teacher=teacher, active=True)
-    context['course'] = course
-
-    return render(request, 'edit_mark_page.html', context)
-
 
 @login_required(login_url='/login')
 def final_report(request, *args, **kwargs):
